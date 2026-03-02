882ded61750a43a0eae0d752aece0a9161e0530f
yupu <yupu@gmail.com>
2026-03-02 11:43:10 +0800

    添加TaskGraphView

--- Assets/Examples/Editor/GraphAssetCallbacks.cs
+++ Assets/Examples/Editor/GraphAssetCallbacks.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 using UnityEditor;
 using GraphProcessor;
 using UnityEditor.Callbacks;
-using System.IO;
 
 public class GraphAssetCallbacks
 {
-	[MenuItem("Assets/Create/GraphProcessor", false, 10)]
-	public static void CreateGraphPorcessor()
-	{
-		var graph = ScriptableObject.CreateInstance< BaseGraph >();
-		ProjectWindowUtil.CreateAsset(graph, "GraphProcessor.asset");
-	}
+    [MenuItem("Assets/Create/GraphProcessor", false, 10)]
+    public static void CreateGraphPorcessor()
+    {
+        var graph = ScriptableObject.CreateInstance<BaseGraph>();
+        ProjectWindowUtil.CreateAsset(graph, "GraphProcessor.asset");
+    }
 
-	[OnOpenAsset(0)]
-	public static bool OnBaseGraphOpened(int instanceID, int line)
-	{
-		var asset = EditorUtility.InstanceIDToObject(instanceID) as BaseGraph;
+    [OnOpenAsset(0)]
+    public static bool OnBaseGraphOpened(int instanceID, int line)
+    {
+        var asset = EditorUtility.InstanceIDToObject(instanceID) as BaseGraph;
 
-		if (asset != null && AssetDatabase.GetAssetPath(asset).Contains("Examples"))
-		{
-			EditorWindow.GetWindow<AllGraphWindow>().InitializeGraph(asset as BaseGraph);
-			return true;
-		}
-		return false;
-	}
+        if (asset != null)
+        {
+            EditorWindow.GetWindow<TaskGraphWindow>().InitializeGraph(asset as BaseGraph);
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ Assets/Examples/Editor/TaskGraph/TaskGraphWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+
+public class TaskGraphWindow : BaseGraphWindow
+{
+    BaseGraph tmpGraph;
+    CustomToolbarView toolbarView;
+
+    [MenuItem("Window/06 Task Graph")]
+    public static BaseGraphWindow OpenWithTmpGraph()
+    {
+        var graphWindow = CreateWindow<TaskGraphWindow>();
+
+        // When the graph is opened from the window, we don't save the graph to disk
+        graphWindow.tmpGraph = ScriptableObject.CreateInstance<BaseGraph>();
+        graphWindow.tmpGraph.hideFlags = HideFlags.HideAndDontSave;
+        graphWindow.InitializeGraph(graphWindow.tmpGraph);
+
+        graphWindow.Show();
+
+        return graphWindow;
+    }
+
+    protected override void OnDestroy()
+    {
+        graphView?.Dispose();
+        DestroyImmediate(tmpGraph);
+    }
+
+    protected override void InitializeWindow(BaseGraph graph)
+    {
+        titleContent = new GUIContent("All Graph");
+
+        if (graphView == null)
+        {
+            graphView = new TaskGraphView(this);
+            toolbarView = new CustomToolbarView(graphView);
+            graphView.Add(toolbarView);
+        }
+
+        rootView.Add(graphView);
+    }
+
+    protected override void InitializeGraphView(BaseGraphView view)
+    {
+        // graphView.OpenPinned< ExposedParameterView >();
+        // toolbarView.UpdateButtonStatus();
+    }
+}
